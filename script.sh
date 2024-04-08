14dc57afd5ebd554b1e1be82e739040d464ac57d
the-worldmainplayer <147723069+the-worldmainplayer@users.noreply.github.com>
2024-04-08 11:06:43 +0200

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Systeminformationen in Datei speichern
+
+# IP-Adresse
+echo "IP-Adresse(n):" > file
+ip addr show | grep 'inet ' | awk '{print $2}' >> file
+
+# MAC-Adresse(n)
+echo -e "\nMAC-Adresse(n):" >> file
+ip link show | awk '/ether/ {print $2}' >> file
+
+# Standard-Gateway
+echo -e "\nStandard-Gateway:" >> file
+ip route | awk '/default/ {print $3}' >> file
+
+# Arbeitsspeicher
+echo -e "\nVerfügbarer Arbeitsspeicher:" >> file
+free -h | grep 'Mem' | awk '{print $2}' | awk '{print "Arbeitsspeicher: " $1}' >> file
+
+# Festplattenspeicher
+echo -e "\nVerfügbarer Festplattenspeicher:" >> file
+df -h | grep '/dev/' | awk '{print $2}' | awk '{print "Festplattenspeicher: " $1}' >> file
+
+# Ausgabe der gesammelten Informationen
+echo "Die gesammelten Systeminformationen wurden in file gespeichert."
